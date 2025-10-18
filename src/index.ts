f76bffc90b556f7923e678a03c5fd6a19293ba21
Nenko Baldzhiev <nenko.baldzhiev@gmail.com>
2025-10-18 18:28:41 +0300

    add openai package

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-// src/index.ts
-console.log("Hello, TypeScript Node Project!");
+import * as OpenAI from 'openai';
+
+const model = 'gpt-4o-mini';
+
+const client = new OpenAI.OpenAI({
+  apiKey: process.env['OPENAI_API_KEY']
+});
+
+const response = await client.responses.create({
+  model,
+  instructions: 'You are a coding assistant that talks like a pirate',
+  input: 'Are semicolons optional in JavaScript?',
+});
+
+console.log(response.output_text);
