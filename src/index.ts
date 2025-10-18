bc35ca9d30eb3003658fecdba9065f377c4737a7
Nenko Baldzhiev <nenko.baldzhiev@gmail.com>
2025-10-18 22:57:30 +0300

    add an option for a random word prompt

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 import * as OpenAI from 'openai';
 
+const modes = ['random word', 'explain'];
 const model = 'gpt-4o-mini';
 const client = new OpenAI.OpenAI({
   apiKey: process.env['OPENAI_API_KEY']
 });
 
-const prompt: string | undefined = process.argv[2]
-if (!prompt) {
-  throw new Error('Please provide a prompt as a command line argument.');
+const mode: string | undefined = process.argv[2]
+const prompt: string | undefined = process.argv[3]
+if (!mode || !modes.includes(mode)) {
+  throw new Error('Please provide a `mode` ("random word | explain") as a command line argument.');
+} else if (mode === 'explain' && !prompt) {
+  throw new Error('Please provide a `prompt` to explain as a second command line argument.');
 }
 
 const response = await client.responses.create({
   model,
-  instructions: 'You are a German tutor. Answer in English.',
-  input: prompt,
+  instructions: 'You are a German tutor. Explain or correct the prompt using English.',
+  input: mode === 'explain' ? prompt! : 'Ask me to translate a random German word, but hide the answer in a dictionary link.',
 });
 
 console.log(response.output_text);
