79bc8f1b058d93d46421dcad01156be782f5d33c
Nenko Baldzhiev <nenko.baldzhiev@gmail.com>
2025-10-18 18:34:35 +0300

    simple textgen script with german tutor instruction

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import * as OpenAI from 'openai';
 
 const model = 'gpt-4o-mini';
-
 const client = new OpenAI.OpenAI({
   apiKey: process.env['OPENAI_API_KEY']
 });
 
+const prompt: string | undefined = process.argv[2]
+if (!prompt) {
+  throw new Error('Please provide a prompt as a command line argument.');
+}
+
 const response = await client.responses.create({
   model,
-  instructions: 'You are a coding assistant that talks like a pirate',
-  input: 'Are semicolons optional in JavaScript?',
+  instructions: 'You are a German tutor. Answer in English.',
+  input: prompt,
 });
 
 console.log(response.output_text);
